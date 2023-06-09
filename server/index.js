52e1d4a6307c0f2cc260458ee320a44084066e54
SIDTK365 <siddharthsingh330@yahoo.in>
2023-06-09 14:19:58 +0530

    Fixes

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const {
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server, { cors: { origin: '*' } })
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 
 app.use(cors())
 app.use(router)
